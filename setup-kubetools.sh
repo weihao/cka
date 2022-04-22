24a33f08608e48fc560e582e5af7fec201c60798
weihao <31719556+weihao@users.noreply.github.com>
2022-04-22 13:11:21 -0400

    Update setup-kubetools.sh

--- setup-kubetools.sh
+++ setup-kubetools.sh
@@ -11,13 +11,8 @@ then
 	exit 3
 fi
 
-# setting MYOS variable
-MYOS=$(hostnamectl | awk '/Operating/ { print $3 }')
-OSVERSION=$(hostnamectl | awk '/Operating/ { print $4 }')
 
 ##### CentOS 7 config
-if [ $MYOS = "CentOS" ]
-then
 	echo RUNNING CENTOS CONFIG
 	cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
@@ -41,28 +36,6 @@ EOF
 	yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
 
 	systemctl enable --now kubelet
-fi
-
-if [ $MYOS = "Ubuntu" ]
-then
-	echo RUNNING UBUNTU CONFIG
-	cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
-	br_netfilter
-EOF
-	
-	sudo apt-get update && sudo apt-get install -y apt-transport-https curl
-	curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-	cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
-	deb https://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-
-	sudo apt-get update
-	sudo apt-get install -y kubelet kubeadm kubectl
-	sudo apt-mark hold kubelet kubeadm kubectl
-	swapoff /swapfile
-	
-	sed -i 's/swapfile/#swapfile/' /etc/fstab
-fi
 
 # Set iptables bridging
 cat <<EOF >  /etc/sysctl.d/k8s.conf
